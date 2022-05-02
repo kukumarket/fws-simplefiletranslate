ebe052f066cd99aaa52c48e279a16a025eb1f18b
霜蓝手环 <kuku2012@163.com>
2022-05-03 00:07:04 +0800

    当前处理功能：后台运行，并接收命令行。

---
+++ server/fws-simple-file-translate-server.py
@@ -0,0 +1,8 @@
+import platform
+print("FWS© kuku2012")
+print("您好。")
+print("您正在运行的软件是 Simple File Translate Server 服务器端")
+print("您的系统为:"+platform.system())
+print("您的平台为:"+platform.platform())
+print("您的设备类型为:"+platform.machine())
+print("您的处理器架构为:"+platform.processor())
